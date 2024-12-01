27acea83c1c6bac18be472c85683afc3430b52dc
BenCurran0 <bmcmorrancurran@gmail.com>
2024-12-01 19:58:28 +0000

    Add files via upload

--- User.java
+++ User.java
@@ -3,6 +3,8 @@ public class User {
     private String username;
     private String password;
     private String role;
+    private double hrsWorked;
+    private int scalePoint;
 
     // Constructor
     public User(String username, String password, String role) {
@@ -24,9 +26,29 @@ public class User {
         return role;
     }
 
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public double getHrsWorked(){
+        return  hrsWorked;
+    }
+
+    public void addHrsWorked(int hrs){
+        hrsWorked =+ hrs;
+    }
+
+    public void setScalePoint(int scalePoint){
+        this.scalePoint = scalePoint;
+    }
+
+    public int getScalePoint(){
+        return scalePoint;
+    }
+
     // To String for saving to CSV
     @Override
     public String toString() {
-        return username + "," + password + "," + role;
+        return username + "," + password + "," + role +","+scalePoint+","+hrsWorked;
     }
 }
